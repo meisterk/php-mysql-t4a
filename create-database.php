e0af99b2419663b280dc5b9bc0a43dfc5aeb6fe4
Franz Kohnle <franz@kohnlehome.de>
2020-04-28 22:54:15 +0200

    Create databses and user

---
+++ create-database.php
@@ -0,0 +1,33 @@
+<?php
+$rootpassword = 'mysql';
+$dbname = 'testdb';
+$dbuser = 'testuser';
+$dbpassword = '123';
+
+// connect to MySQL as MySQL-Root
+$pdo = new PDO(
+    "mysql:host=localhost",
+    'root',
+    $rootpassword
+);
+// show errors
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// create database
+echo 'Try to create database ...<br>';
+$sql = "DROP DATABASE $dbname";
+$pdo->exec($sql);
+$sql = "CREATE DATABASE $dbname";
+$pdo->exec($sql);
+
+// create user
+echo 'Try to create user ...<br>';
+$sql = "DROP USER IF EXISTS '$dbuser'@'localhost'";
+$pdo->exec($sql);
+$sql = "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpassword'";
+$pdo->exec($sql);
+
+// create permissions for user
+echo 'Try to create permissions ...<br>';
+$sql = "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost'";
+$pdo->exec($sql);
