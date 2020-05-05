7390e5e5a49708eb4341404f6f292d172ed5f18e
Franz Kohnle <franz@kohnlehome.de>
2020-05-05 15:13:21 +0200

    Add file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schülerverwaltung</title>
+</head>
+
+<body>
+    <h1>Schülerverwaltung</h1>
+</body>
+
+</html>
