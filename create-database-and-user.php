6d6d4abb134bfabd333daa3bb7585d03ffeba0da
Franz Kohnle <franz@kohnlehome.de>
2020-05-05 14:45:48 +0200

    create user

--- create-database-and-user.php
+++ create-database-and-user.php
@@ -2,6 +2,8 @@
 // DATA
 $rootpassword = 'mysql';
 $databasename = 'testdatabase';
+$username = 'testuser';
+$password = '123';
 
 // Root-Connection to MySQL/MariaDB
 $pdo = new PDO(
@@ -14,9 +16,17 @@ $pdo = new PDO(
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // CREATE DATABASE
-echo 'TRY to create Database ...';
+echo 'TRY to create database ...<br>';
 $sql = "DROP DATABASE IF EXISTS $databasename";
 $pdo->exec($sql);
 
 $sql = "CREATE DATABASE $databasename";
+$pdo->exec($sql);
+
+// CREATE USER
+echo 'TRY to create user ...<br>';
+$sql = "DROP USER IF EXISTS '$username'@'localhost'";
+$pdo->exec($sql);
+
+$sql = "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'";
 $pdo->exec($sql);
