1b1c5d726d48a83e41d32ed47c8cd71cc7d24d8e
Franz Kohnle <franz@kohnlehome.de>
2020-05-05 15:27:46 +0200

    Display table

--- index.php
+++ index.php
@@ -9,6 +9,36 @@
 
 <body>
     <h1>Schülerverwaltung</h1>
+    <table>
+        <tr>
+            <th>Vorname</th>
+            <th>Nachname</th>
+        </tr>
+
+        <?php
+        // DATA
+        $databasename = 'testdatabase';
+        $username = 'testuser';
+        $password = '123';
+
+        // Root-Connection to MySQL/MariaDB
+        $pdo = new PDO(
+            "mysql:host=localhost;dbname=$databasename",
+            $username,
+            $password
+        );
+
+        // Show errors
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT * FROM schueler";
+        foreach ($pdo->query($sql) as $zeile) {
+            $vorname = $zeile['vorname'];
+            $nachname = $zeile['nachname'];
+            echo "<tr><td>$vorname</td><td>$nachname</td></tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
